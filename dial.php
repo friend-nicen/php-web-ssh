0e7b019ac4f692af594345f20e277fae6e0cadd3
friend-nicen <1747111677@qq.com>
2023-03-30 10:46:01 +0000

    update dial.php.
    Signed-off-by: friend-nicen <1747111677@qq.com>

--- dial.php
+++ dial.php
@@ -29,13 +29,10 @@ Co::set([
 ]);
 
 
-
-
 /*
  * 创建协程容器
  * */
 run(function () {
-
 
 
     /*
@@ -44,7 +41,7 @@ run(function () {
     go(function () {
         Timer::tick(1000, function () {
             $size = gc_mem_caches();
-            if($size>0){
+            if ($size > 0) {
                 logs("内存释放" . $size);
             }
 
@@ -52,93 +49,119 @@ run(function () {
     });
 
 
-
     /*
      * 第三个参数 是否开启ssl
      * */
     $server = new Server('0.0.0.0', 5017, false);
-    $server->table = []; //共享内存
-    $server->redis = getRedis(); //创建redis
-    $server->redis->del("ONLINE_STAFF"); //清空在线状态
 
-
-
-    /*
-     * redi
-     * */
-    go(function () use ($server) {
-
-
-        $redis = getRedis(); //创建redis
-
-
-        logs('Redis订阅已启动！');
-
-
-        /*
-         * 开启消息订阅
-         * */
-        $redis->subscribe(["RECV_DIAL"], function ($redis, $chan, $msg) use ($server) {
-
-            logs('新消息：' . $msg);
-
-            $json = @json_decode($msg, true); //redis消息
-            if (empty($json['name'])) return; //如果手机号为空
-
-            $ws = $server->table[$json['name']]; //websocket服务器
-
-            /* 如果ws不存在 */
-            if (!$ws) {
-                logs('未获取到在线的websocket：' . $msg);
-                return;
-            }
-
-
-            /*
-             * 消息推送
-             * */
-            switch ($json['type']) {
-                case 'speak':
-                    $ws->push($json['speak']);
-                    logs("拨打电话：" . $json['speak'] . "！");
-                    break;
-                case 'quit':
-                    $ws->is_closed = true; //标记关闭
-                    $ws->push('quit');
-                    break;
-                case 'message':
-                    $ws->push("#" . $json['speak']);
-                    logs("发送短信：" . $json['speak'] . "！");
-                    break;
-            }
-
-
-        });
-
-
-    });
-
-
-    $server->handle('/ws', function (Request $request, Response $ws) use ($server) {
+    $server->handle('/ws', function (Request $request, Response $ws) {
 
         /*websocket协议*/
         $ws->upgrade();
 
+
         /*
-         * 断开连接，清理退出
+         * 创建协程，并获取Guid
          * */
-        $quit = function ($log) use ($ws, $server) {
+        $subscribe = function () use ($ws) {
 
-            logs(($ws->mobile ?? "未登录") . "：" . "断开连接：" . $log);//记录退出原因
-            logs("当前Ws连接数量：" . count($server->table));
+            /* 保存协程ID */
+            $ws->Gid = go(function () use ($ws) {
 
-            /* 是否要清除数据 */
-            if (!isset($ws->is_closed) && $ws->mobile) {
+                $redis = getRedis(); //创建redis
 
-                unset($server->table[$ws->mobile]);
-                $ws->redis->hDel("ONLINE_STAFF", $ws->mobile); //清除在线状态
-                $ws->redis->close(); //关闭redis
+                /*
+                 * 协程退出时清理
+                 * */
+                defer(function () use ($redis, $ws) {
 
+                    /*
+                     * 退出
+                     * */
+                    logs($ws->mobile . '，已断开链接！');
+
+                    if(!$ws->is_closed){
+                        $redis->hDel("ONLINE_STAFF", $ws->mobile); //取消在线状态
+                    }
+
+                    $redis->rawCommand("UNSUBSCRIBE", "RECV_DIAL_" . $ws->mobile);//取消订阅
+                    $redis->close();//关闭链接，导致订阅抛出异常
+                });
+
+                /*
+                 * close时，将抛出异常
+                 * */
+                try {
+                    /*
+                     * 订阅频道，当http api处理完毕时将给
+                     * */
+                    logs($ws->mobile . '，已上线！');
+
+                    /*
+                     * 记录已上线
+                     * */
+                    $redis->hSet('ONLINE_STAFF', $ws->mobile, time());
+
+                    /*
+                     * 开启消息订阅
+                     * */
+                    $redis->subscribe(["RECV_DIAL_" . $ws->mobile], function ($redis, $chan, $msg) use ($ws) {
+
+
+                        logs('新消息：' . $msg);
+
+                        $json = @json_decode($msg, true); //redis消息
+                        if (empty($json['name'])) return; //如果手机号为空
+
+
+                        /*
+                         * 消息推送
+                         * */
+                        switch ($json['type']) {
+                            case 'speak':
+                                $ws->push($json['speak']);
+                                logs("拨打电话：" . $json['speak'] . "！");
+                                break;
+                            case 'quit':
+                                $ws->is_closed = true; //标记关闭
+                                $ws->push('quit');
+                                break;
+                            case 'message':
+                                $ws->push("#" . $json['speak']);
+                                logs("发送短信：" . $json['speak'] . "！");
+                                break;
+                        }
+
+
+                    });
+
+                } catch (\Throwable $e) {
+                    logs($ws->mobile . '，订阅已经关闭');
+                }
+
+            });
+        };
+
+
+        /*
+         * 清理链接
+         * */
+        $quit = function ($log) use ($ws) {
+
+            logs($log);//记录退出原因
+
+            /*
+             * 如果协程已经运行
+             * */
+            if (isset($ws->Gid)) {
+                Coroutine::cancel($ws->Gid); //关闭协程
+            }
+
+            /*
+             * 关闭redis链接
+             * */
+            if ($ws->redis) {
+                $ws->redis->close();//关闭链接
             }
 
             $ws->close(); //断开ws
@@ -146,9 +169,6 @@ run(function () {
         };
 
 
-        /*
-         * 时间循环
-         * */
         while (true) {
 
             $frame = $ws->recv(); //阻塞接收消息
@@ -200,15 +220,17 @@ run(function () {
                         if (!empty($data['msg']['name'])) {
 
 
+                            logs($data['msg']['name'] . "登录状态：" . $ws->redis->hGet("ONLINE_STAFF", $data['msg']['name']));
+
                             /*
                              * 如果已经在线
                              * */
-                            if (!empty($server->table[$data['msg']['name']])) {
+                            if ($ws->redis->hGet("ONLINE_STAFF", $data['msg']['name'])) {
 
                                 logs("禁止重复登录，通知" . $data['msg']['name'] . "自动退出！");//记录退出原因
 
                                 /* 通知退出 */
-                                $ws->redis->publish("RECV_DIAL", json_encode([
+                                $ws->redis->publish("RECV_DIAL_" . $data['msg']['name'], json_encode([
                                     'type' => 'quit',
                                     'name' => $data['msg']['name']
                                 ]));
@@ -218,21 +240,16 @@ run(function () {
                             logs('新消息：' . $data['msg']['name'] . "上线");
 
 
-                            /* 记录连接 */
-                            $server->table[$data['msg']['name']] = $ws;
-                            /* 标记手机号 */
-                            $ws->mobile = $data['msg']['name'];
-                            /* 记录登录状态 */
-                            $ws->redis->hSet('ONLINE_STAFF', $data['msg']['name'], time());
+                            $ws->mobile = $data['msg']['name']; //保存手机号
+                            $subscribe(); //开始订阅
                         }
 
                     } else {
-                        $ws->redis->publish("RECV_DIAL", $frame->data);
+                        $ws->redis->publish("RECV_DIAL_" . $ws->mobile, $frame->data);
                     }
 
 
                 }
-
 
             }
         }
@@ -243,7 +260,7 @@ run(function () {
      * 输出默认测试模板
      * */
     $server->handle('/', function (Request $request, Response $response) {
-        $response->end(getTestB());
+        $response->end(getTest());
     });
 
 
